61ac3815fffaf0d58ed8a3b1acc354f9773a0bee
RND-HW3 <RND-HW3@RND-HW3>
2025-05-07 16:38:12 +0900

    초기 커밋.

---
+++ TryCatch/MainApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            try
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write($"{arr[i]} ");
+                }                
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"예외 발생: {e.Message}");
+            }
+            
+            Console.WriteLine("종료");
+        }
+    }
+}
